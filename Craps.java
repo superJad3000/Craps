14c2fd8a10a0a9d96e78c4cfa4178894b21446b7
Richard White <rwhite@crashwhite.com>
2021-02-18 09:36:38 -0800

    Added comments from instructor

--- Craps.java
+++ Craps.java
@@ -78,3 +78,22 @@ public class Craps
 
     }
 }
+
+
+/* 
+
+COMMENTS FROM THE INSTRUCTOR:
+
+This program does a nice job of displaying the basic functionality
+of a Craps game, Jack. Well done!
+
+You didn't implement all of the features asked for in the handout--
+a default Y/n feature, for example--and where are your @author JavaDocs
+for this file? How am I supposed to know to give you credit here? :)
+
+Overall, not bad, though!
+
+SCORE: 13/15
+
+*/
+
